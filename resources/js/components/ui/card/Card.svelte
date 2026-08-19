1c50b19ef6bb467032ae1ce2ee70edf9eb28085e
bassam <bssamkh8009@gmail.com>
2026-08-19 15:47:21 +0300

    add shadcn compomets

--- resources/js/components/ui/card/Card.svelte
+++ resources/js/components/ui/card/Card.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
-    import type { Snippet } from 'svelte';
-    import { cn } from '@/lib/utils';
+	import { cn, type WithElementRef } from "@/lib/utils.js";
+	import type { HTMLAttributes } from "svelte/elements";
 
-    let {
-        class: className = '',
-        children,
-    }: {
-        class?: string;
-        children?: Snippet;
-    } = $props();
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		size = "default",
+		...restProps
+	}: WithElementRef<HTMLAttributes<HTMLDivElement>> & { size?: "default" | "sm" } = $props();
 </script>
 
-<div data-slot="card" class={cn('bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm', className)}>
-    {@render children?.()}
+<div
+	bind:this={ref}
+	data-slot="card"
+	data-size={size}
+	class={cn("gap-(--card-spacing) overflow-hidden rounded-xl bg-card py-(--card-spacing) text-sm text-card-foreground ring-1 ring-foreground/10 [--card-spacing:--spacing(4)] has-data-[slot=card-footer]:pb-0 has-[>img:first-child]:pt-0 data-[size=sm]:[--card-spacing:--spacing(3)] data-[size=sm]:has-data-[slot=card-footer]:pb-0 *:[img:first-child]:rounded-t-xl *:[img:last-child]:rounded-b-xl group/card flex flex-col", className)}
+	{...restProps}
+>
+	{@render children?.()}
 </div>
